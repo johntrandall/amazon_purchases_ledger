bfd9b250a5f517f6146cdb30c347f0cb23a09e17
John Randall <john@johnrandall.com>
2018-03-23 22:04:14 -0400

    add Thor gem

--- lib/amazon_csv_combiner.rb
+++ lib/amazon_csv_combiner.rb
@@ -1,5 +1,19 @@
 require "amazon_csv_combiner/version"
 
+require 'thor'
+
 module AmazonCsvCombiner
   # Your code goes here...
+  class CLI < Thor
+    desc "hello world", "my first cli yay"
+    def hello(name)
+      if name == "Heisenberg"
+        puts "you are goddman right"
+      else
+        puts "say my name"
+      end
+    end
+
+  end
 end
+
