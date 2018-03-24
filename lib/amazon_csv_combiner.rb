2d46109bfa7ae67cc9adf9af5df499a8182b2caf
John Randall <john@johnrandall.com>
2018-03-23 20:56:14 -0400

    bundler init

---
+++ lib/amazon_csv_combiner.rb
@@ -0,0 +1,5 @@
+require "amazon_csv_combiner/version"
+
+module AmazonCsvCombiner
+  # Your code goes here...
+end
---
+++ spec/amazon_csv_combiner_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AmazonCsvCombiner do
+  it "has a version number" do
+    expect(AmazonCsvCombiner::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
