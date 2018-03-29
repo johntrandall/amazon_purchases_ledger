3410923bb6f7ab2aa618600ef6ca34d7ee8aed4b
John Randall <john@johnrandall.com>
2018-03-29 01:47:40 -0400

    truncate Item#title; delete unused objects

--- lib/amazon_csv_combiner/item.rb
+++ lib/amazon_csv_combiner/item.rb
@@ -20,15 +20,44 @@ module AmazonCsvCombiner
     def output_text
       [
         '* ',
-        @items_csv_row[:item_total], ' for ',
-        item_quantity_string,
-        @items_csv_row[:title], ', ',
-        'Seller: ', @items_csv_row[:seller]
+        item_cost, ' for ',
+        item_quantity_string, item_title, ', ',
+        'Seller: ', item_seller
       ].join
     end
 
     def item_quantity_string
       "#{@items_csv_row[:quantity]}x " if @items_csv_row[:quantity]
+    end
+
+    private
+    def item_seller
+      @items_csv_row[:seller]
+    end
+
+    def item_title
+      truncate(@items_csv_row[:title], 40)
+    end
+
+    def item_cost
+      @items_csv_row[:item_total]
+    end
+
+    def truncate(content, max)
+      if content.length > max
+        truncated = ""
+        collector = ""
+        content = content.split(" ")
+        content.each do |word|
+          word = word + " "
+          collector << word
+          truncated << word if collector.length < max
+        end
+        truncated = truncated.strip.chomp(",").concat("...")
+      else
+        truncated = content
+      end
+      return truncated
     end
 
   end
